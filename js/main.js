10aeff2d025f7411a0cf2a3873e008abf68fa509
Abdelrahman <abdelrahmanawad.dev@gmail.com>
2023-02-03 04:11:44 +0200

    solve conflict

--- js/main.js
+++ js/main.js
@@ -1,17 +1,4 @@
-// let icon = document.getElementById("icon");
-// let icon1 = document.getElementById("a");
-// let icon2 = document.getElementById("b");
-// let icon3 = document.getElementById("c");
-// let nav = document.getElementById("nav");
-// let blue = document.getElementById("blue");
-
-// icon.addEventListener("click", function () {
-//     icon1.classList.toggle("a");
-//     icon2.classList.toggle("c");
-//     icon3.classList.toggle("b");
-//     nav.classList.toggle("show");
-//     blue.classList.toggle("slide");
-// });
+// navbar button in mobile viewport  js code
 
 let navBtn = document.querySelector(".dropDown");
 let myList = document.querySelector(".links");
@@ -24,6 +11,8 @@ navBtn.onclick = function () {
         document.querySelector("body").style.overflow = "auto";
     }
 };
+
+//  navbar color chang in scrolling js code
 
 let navbar = document.querySelector("header");
 
@@ -46,3 +35,22 @@ navTransition();
 window.onscroll = function () {
     navTransition();
 };
+
+//  OUR JOURNAL slider js code
+
+var swiper = new Swiper(".mySwiper", {
+    effect: "coverflow",
+    grabCursor: true,
+    centeredSlides: true,
+    slidesPerView: "auto",
+    coverflowEffect: {
+        rotate: 50,
+        stretch: 0,
+        depth: 100,
+        modifier: 1,
+        slideShadows: true,
+    },
+    pagination: {
+        el: ".swiper-pagination",
+    },
+});
